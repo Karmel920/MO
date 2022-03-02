15dde662c8729bef8ef605419ee2f508fdbe4a0a
Mikolaj Petecki <mikolajpe2@gmail.com>
2022-03-02 10:44:15 +0100

    Start project

--- Zad1/main.cpp
+++ Zad1/main.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world";
+    /*
+     * espylon = 2ni = 2^(-t)
+     * x- <---> x <---> x+
+     * log10((fp(x)-f(x)/f(x)) -|- log10(x)
+     */
+    int t = 0;
+    double epsylon;
+    double stala = 1.0;
+
+    epsylon = 1.0;
+
+    while(1 + epsylon > stala) {
+        epsylon /= 2.0;
+        t += 1;
+    }
+
+    cout << t << endl;
+    cout << epsylon;
+
     return 0;
 }
+
+
